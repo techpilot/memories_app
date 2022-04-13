4975b3e4571f4b7e913623a8f89ecca93f8f3b2e
Stephen Ngwu <ngwustephen99@gmail.com>
2022-04-13 05:33:12 +0100

    initialized memories app. CRUD for both server and client

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+import { AppBar, Avatar, Typography, Toolbar, Button } from "@material-ui/core"
+
+import memories from "../../images/memories.png"
+import useStyles from "./styles"
+
+const Navbar = () => {
+  const classes = useStyles();
+
+  const user = null
+
+  return (
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <div className={classes.brandContainer}>
+        <Typography component={Link} to="/" className={classes.heading} variant="h3" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="memories" height="50" />
+      </div>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+            <Button className={classes.logout} variant="contained" color="secondary">Logout</Button>
+          </div>
+        ) : (
+          <Button component={Link} to="/auth" variant="contained" color="primary">Sign in</Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Navbar
